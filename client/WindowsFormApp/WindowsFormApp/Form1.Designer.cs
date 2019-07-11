cea967745662dd53847db53305ead39c345edbe1
haondt <burghardtnoah@gmail.com>
2019-07-10 23:10:09 -0600

    added client

---
+++ client/WindowsFormApp/WindowsFormApp/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HWIDLabel = new System.Windows.Forms.Label();
+            this.HWIDTextBox = new System.Windows.Forms.TextBox();
+            this.HWIDButton = new System.Windows.Forms.Button();
+            this.LicenseLabel = new System.Windows.Forms.Label();
+            this.LicenseKeyTextBox = new System.Windows.Forms.TextBox();
+            this.LicenseKeyButton = new System.Windows.Forms.Button();
+            this.SignatureVerificationLabel = new System.Windows.Forms.Label();
+            this.HWIDVerificationLabel = new System.Windows.Forms.Label();
+            this.LoadLicenseFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HWIDLabel
+            // 
+            this.HWIDLabel.AutoSize = true;
+            this.HWIDLabel.Location = new System.Drawing.Point(146, 94);
+            this.HWIDLabel.Name = "HWIDLabel";
+            this.HWIDLabel.Size = new System.Drawing.Size(40, 13);
+            this.HWIDLabel.TabIndex = 0;
+            this.HWIDLabel.Text = "HWID:";
+            this.HWIDLabel.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // HWIDTextBox
+            // 
+            this.HWIDTextBox.Location = new System.Drawing.Point(187, 91);
+            this.HWIDTextBox.Name = "HWIDTextBox";
+            this.HWIDTextBox.Size = new System.Drawing.Size(380, 20);
+            this.HWIDTextBox.TabIndex = 1;
+            // 
+            // HWIDButton
+            // 
+            this.HWIDButton.Location = new System.Drawing.Point(573, 89);
+            this.HWIDButton.Name = "HWIDButton";
+            this.HWIDButton.Size = new System.Drawing.Size(120, 23);
+            this.HWIDButton.TabIndex = 2;
+            this.HWIDButton.Text = "Get HWID";
+            this.HWIDButton.UseVisualStyleBackColor = true;
+            this.HWIDButton.Click += new System.EventHandler(this.HWIDButton_Click);
+            // 
+            // LicenseLabel
+            // 
+            this.LicenseLabel.AutoSize = true;
+            this.LicenseLabel.Location = new System.Drawing.Point(118, 120);
+            this.LicenseLabel.Name = "LicenseLabel";
+            this.LicenseLabel.Size = new System.Drawing.Size(68, 13);
+            this.LicenseLabel.TabIndex = 0;
+            this.LicenseLabel.Text = "License Key:";
+            this.LicenseLabel.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // LicenseKeyTextBox
+            // 
+            this.LicenseKeyTextBox.Enabled = false;
+            this.LicenseKeyTextBox.Location = new System.Drawing.Point(187, 117);
+            this.LicenseKeyTextBox.Name = "LicenseKeyTextBox";
+            this.LicenseKeyTextBox.Size = new System.Drawing.Size(380, 20);
+            this.LicenseKeyTextBox.TabIndex = 1;
+            // 
+            // LicenseKeyButton
+            // 
+            this.LicenseKeyButton.Location = new System.Drawing.Point(306, 143);
+            this.LicenseKeyButton.Name = "LicenseKeyButton";
+            this.LicenseKeyButton.Size = new System.Drawing.Size(120, 23);
+            this.LicenseKeyButton.TabIndex = 2;
+            this.LicenseKeyButton.Text = "Verify License Key";
+            this.LicenseKeyButton.UseVisualStyleBackColor = true;
+            this.LicenseKeyButton.Click += new System.EventHandler(this.LicenseKeyButton_Click);
+            // 
+            // SignatureVerificationLabel
+            // 
+            this.SignatureVerificationLabel.AutoSize = true;
+            this.SignatureVerificationLabel.Location = new System.Drawing.Point(353, 169);
+            this.SignatureVerificationLabel.Name = "SignatureVerificationLabel";
+            this.SignatureVerificationLabel.Size = new System.Drawing.Size(35, 13);
+            this.SignatureVerificationLabel.TabIndex = 3;
+            this.SignatureVerificationLabel.Text = "label1";
+            // 
+            // HWIDVerificationLabel
+            // 
+            this.HWIDVerificationLabel.AutoSize = true;
+            this.HWIDVerificationLabel.Location = new System.Drawing.Point(353, 191);
+            this.HWIDVerificationLabel.Name = "HWIDVerificationLabel";
+            this.HWIDVerificationLabel.Size = new System.Drawing.Size(35, 13);
+            this.HWIDVerificationLabel.TabIndex = 3;
+            this.HWIDVerificationLabel.Text = "label1";
+            // 
+            // LoadLicenseFileButton
+            // 
+            this.LoadLicenseFileButton.Location = new System.Drawing.Point(573, 117);
+            this.LoadLicenseFileButton.Name = "LoadLicenseFileButton";
+            this.LoadLicenseFileButton.Size = new System.Drawing.Size(120, 23);
+            this.LoadLicenseFileButton.TabIndex = 2;
+            this.LoadLicenseFileButton.Text = "Load License File";
+            this.LoadLicenseFileButton.UseVisualStyleBackColor = true;
+            this.LoadLicenseFileButton.Click += new System.EventHandler(this.LoadLicenseFileButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 351);
+            this.Controls.Add(this.HWIDVerificationLabel);
+            this.Controls.Add(this.SignatureVerificationLabel);
+            this.Controls.Add(this.LicenseKeyButton);
+            this.Controls.Add(this.LoadLicenseFileButton);
+            this.Controls.Add(this.HWIDButton);
+            this.Controls.Add(this.LicenseKeyTextBox);
+            this.Controls.Add(this.LicenseLabel);
+            this.Controls.Add(this.HWIDTextBox);
+            this.Controls.Add(this.HWIDLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HWIDLabel;
+        private System.Windows.Forms.TextBox HWIDTextBox;
+        private System.Windows.Forms.Button HWIDButton;
+        private System.Windows.Forms.Label LicenseLabel;
+        private System.Windows.Forms.TextBox LicenseKeyTextBox;
+        private System.Windows.Forms.Button LicenseKeyButton;
+        private System.Windows.Forms.Label SignatureVerificationLabel;
+        private System.Windows.Forms.Label HWIDVerificationLabel;
+        private System.Windows.Forms.Button LoadLicenseFileButton;
+    }
+}
+
---
+++ client/WindowsFormApp/WindowsFormApp/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Security.Cryptography;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.OpenSsl;
+
+namespace WindowsFormApp
+{
+    public partial class Form1 : Form
+    {
+        string publicKey;
+        string signature;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.HWIDVerificationLabel.Text = "";
+            this.SignatureVerificationLabel.Text = "";
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private string getHWID()
+        {
+            return new ManagementObjectSearcher("SELECT SerialNumber FROM Win32_OperatingSystem").Get().Cast<ManagementObject>().First()["SerialNumber"].ToString();
+        }
+
+        private void HWIDButton_Click(object sender, EventArgs e)
+        {
+            this.HWIDTextBox.Text = this.getHWID();
+        }
+
+        private void LicenseKeyButton_Click(object sender, EventArgs e)
+        {
+            if (this.signature == null)
+            {
+                this.HWIDVerificationLabel.Text = "No license loaded!";
+                return;
+            }
+
+            string HWID = this.HWIDTextBox.Text;
+            this.publicKey = this.getPublicKey();
+
+            // Verify HWID matches license key
+            if (this.verifyHWID(HWID, this.LicenseKeyTextBox.Text))
+            {
+                this.HWIDVerificationLabel.Text = "HWID matches license key";
+                this.HWIDVerificationLabel.ForeColor = Color.Green;
+            }
+            else
+            {
+                this.HWIDVerificationLabel.Text = "HWID does not match license key";
+                this.HWIDVerificationLabel.ForeColor = Color.Red;
+            }
+
+
+            // Verify signature
+            if (this.verifyLicenseSignature(this.LicenseKeyTextBox.Text, this.publicKey, this.signature))
+            {
+                this.SignatureVerificationLabel.Text = "Signature matches license key";
+                this.SignatureVerificationLabel.ForeColor = Color.Green;
+            }
+            else
+            {
+
+                this.SignatureVerificationLabel.Text = "Signature does not match license key";
+                this.SignatureVerificationLabel.ForeColor = Color.Red;
+            }
+    
+
+
+        }
+
+        private string getPublicKey()
+        {
+            return File.ReadAllText(@"C:\Users\Noah Burghardt\Desktop\license_server\pyserver\pbKey.txt");
+        }
+
+        private bool verifyHWID(string HWID, string licenseKey)
+        {
+            string HWIDHash;
+            using(System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(HWID);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                HWIDHash = hashBytes.Aggregate(new StringBuilder(), (SB, hb) => SB.Append(hb.ToString("x2"))).ToString();
+            }
+            return HWIDHash.CompareTo(licenseKey) == 0;
+        }
+
+        private void LoadLicenseFileButton_Click(object sender, EventArgs e)
+        {
+            // License key that is valid, matches computer HWID and has matching signature
+            string licenseJson = this.getLicenseKeyJson(@"C:\Users\Noah Burghardt\Desktop\license_server\pyserver\good_license.txt");
+
+            // License key that was created for a different computer, and they just manually changed the HWID in the license file
+            //string licenseJson = this.getLicenseKeyJson(@"C:\Users\Noah Burghardt\Desktop\license_server\pyserver\tampered_license.txt");
+
+            // License key that is valid but was generated for a different computer
+            //string licenseJson = this.getLicenseKeyJson(@"C:\Users\Noah Burghardt\Desktop\license_server\pyserver\HWID_license.txt");
+
+            // License key that matches computer HWID and has matching signature but was invalidated by the server
+            //string licenseJson = this.getLicenseKeyJson(@"C:\Users\Noah Burghardt\Desktop\license_server\pyserver\invalid_license.txt");
+
+            JObject license = JObject.Parse(licenseJson);
+            this.LicenseKeyTextBox.Text = license["license_key"].ToString();
+            this.signature = license["signature"].ToString();
+        }
+
+        private string getLicenseKeyJson(string location)
+        {
+            return File.ReadAllText(location);
+        }
+
+        private bool verifyLicenseSignature(string licenseKey, string publicKey, string signature)
+        {
+            byte[] encodedPublicKey = Encoding.UTF8.GetBytes(publicKey);
+            byte[] encodedLicenseKey = Encoding.UTF8.GetBytes(licenseKey);
+            byte[] decodedSignature = Convert.FromBase64String(signature);
+
+
+
+            SHA256 sha256 = new SHA256Managed();
+            PemReader pr = new PemReader(new StringReader(publicKey));
+            AsymmetricKeyParameter publicKeyParameter = (AsymmetricKeyParameter)pr.ReadObject();
+            RSAParameters rsaParams = DotNetUtilities.ToRSAParameters((RsaKeyParameters)publicKeyParameter);
+
+            RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(rsaParams);
+
+            return csp.VerifyData(encodedLicenseKey, CryptoConfig.MapNameToOID("SHA256"), decodedSignature);
+
+            
+
+
+
+        }
+    }
+}
